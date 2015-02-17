18696719273cd5c631f19d872d95df5bc1ec62ed
Jaroslav <Jaroslav@Lenovo-PC>
2015-02-17 22:41:54 +0100

    Initial Commit

---
+++ src/smartcat/SmartCat.java
@@ -0,0 +1,16 @@
+package smartcat;
+
+/**
+ *
+ * @author me
+ */
+public class SmartCat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("meow");
+    }
+    
+}
